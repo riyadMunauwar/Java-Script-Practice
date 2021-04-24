4fbfc1679ba608824a6b8353b02283cc3f138a8f
riyad <hifazakhatun1108@gmail.com>
2021-04-24 13:45:30 +0600

    store add

---
+++ eventBasedUpdate.js
@@ -0,0 +1,60 @@
+EventBasedUpdate.prototype.setStore = function(store){this.store = store;};
+
+EventBasedUpdate.prototype.select = function(el){
+  return document.getElementById(el);
+}
+
+EventBasedUpdate.prototype.onEvent = function(el,event, handeler){
+  this.select(el).addEventListener(event, handeler);
+}
+
+EventBasedUpdate.prototype.registerComponent = function(component){
+  this.components.push(component);
+}
+
+EventBasedUpdate.prototype.renderUi = function(){
+  var _this = this;
+  this.components.forEach(function(ui){
+    for(var handeler in ui){
+      ui[handeler](_this.store);
+    }
+  })
+  
+}
+
+EventBasedUpdate.prototype.on = function(el,event, handeler){
+  this.onEvent(el,event, handeler);
+  this.renderUi();
+}
+
+EventBasedUpdate.prototype.init = function(){
+  this.renderUi();
+}
+
+
+
+var app = new EventBasedUpdate();
+
+app.setStore(50)
+
+
+app.registerComponent({name: (s) => app.changeState((s)=> {console.log('change');}), age: (s)=> console.log('age+s')})
+
+
+
+app.init();
+
+app.on('btn-1', 'click', () => {
+  app.renderUi();
+});
+
+
+
+
+
+
+
+
+
+
+
